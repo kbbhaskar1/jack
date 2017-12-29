0f677ba7d9b1dd7f14b142a2281e46a94011e187
kbbhaskar1 <33472006+kbbhaskar1@users.noreply.github.com>
2017-12-29 11:36:18 -0500

    added log

--- jackrabbit/src/main/java/FirstHop.java
+++ jackrabbit/src/main/java/FirstHop.java
@@ -22,7 +22,7 @@ public class FirstHop {
         try { 
             String user = session.getUserID(); 
             String name = repository.getDescriptor(Repository.REP_NAME_DESC); 
-            System.out.println( 
+            System.out.println( "first hop"+
             "Logged in as " + user + " to a " + name + " repository."); 
         } finally { 
             session.logout(); 
